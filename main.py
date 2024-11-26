491f3556177200f61f4f5558c14d6208567936f1
Treecher <martin.vrobel308@gmail.com>
2024-11-26 17:10:32 +0100

    renamed things

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def main():
     pygame.init()
 
     pygame.display.set_caption("Rogue-Like game")
-    okno_sirka = 800
-    okno_vyska = 600
-    okno = pygame.display.set_mode((okno_sirka, okno_vyska))
+    width = 800
+    height = 600
+    okno = pygame.display.set_mode((width, height))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
