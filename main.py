115b649412aef8c71e6340faba5a1404b708d3f9
Treecher <martin.vrobel308@gmail.com>
2024-11-26 17:19:31 +0100

    added simple animati or whatever idk

--- main.py
+++ main.py
@@ -2,15 +2,31 @@ import pygame
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+
+    x = 0
+    y = 0
+    z = 0
 
     pygame.display.set_caption("Rogue-Like game")
     width = 800
     height = 600
-    okno = pygame.display.set_mode((width, height))
+    window = pygame.display.set_mode((width, height))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+        
+        xyz = (x % 256,y % 256,z % 256)
+
+        window.fill(xyz)
+
+        x += 1
+        z += 1
+        y += 1
+
+        pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
