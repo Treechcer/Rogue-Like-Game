468a960197c313f570ebd15c0decd4d31f9a53cd
Treecher <martin.vrobel308@gmail.com>
2024-11-26 20:43:02 +0100

    made "level" loader and functional moving and other stuff

--- inputHandler.py
+++ inputHandler.py
@@ -3,16 +3,20 @@ import pygame
 def inputHandler(position):
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        position[0] -= 1
+        if position[0] != 0:
+            position[0] -= 1
         return position
     elif keys[pygame.K_RIGHT]:
-        position[0] += 1
+        if position[0] < 15:
+            position[0] += 1
         return position
     elif keys[pygame.K_UP]:
-        position[1] -= 1
+        if position[1] != 0:
+            position[1] -= 1
         return position
     elif keys[pygame.K_DOWN]:
-        position[1] += 1
+        if position[1] < 11:
+            position[1] += 1
         return position
     else:
         return position
