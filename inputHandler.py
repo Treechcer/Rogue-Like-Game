98646f932763491079e8fbb372eadac13e8114c9
Treecher <martin.vrobel308@gmail.com>
2024-11-26 18:14:04 +0100

    made it a "game"

---
+++ inputHandler.py
@@ -0,0 +1,18 @@
+import pygame
+
+def inputHandler(position):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        position[0] -= 1
+        return position
+    elif keys[pygame.K_RIGHT]:
+        position[0] += 1
+        return position
+    elif keys[pygame.K_UP]:
+        position[1] -= 1
+        return position
+    elif keys[pygame.K_DOWN]:
+        position[1] += 1
+        return position
+    else:
+        return position
