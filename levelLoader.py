d4233d0b7cabb6e5a226bfde10f3374690ec3ac1
Treecher <martin.vrobel308@gmail.com>
2024-11-27 21:48:09 +0100

    made enemy AI (with almost everything)

--- levelLoader.py
+++ levelLoader.py
@@ -5,30 +5,38 @@ tile = 48
 def loadLevel(window, level, frameCount, position):
     wall = []
     for row in range(16):
-        for col in range(11):
+        for col in range(12):
             if level[col][row] == "x":
                 pygame.draw.rect(window, (0,0,50), (row * tile, col * tile, tile, tile))
-            elif level[col][row] == "y":
-                pygame.draw.rect(window, (10,200,10), (row * tile, col * tile, tile, tile))
             elif level[col][row] == "w":
-                tile = 48
-                image = pygame.image.load("sprites/wall.png")
-                image = pygame.transform.scale(image, (tile, tile))
-                window.blit(image, (row * tile, col * tile))
+                drawObj(row, col, "sprites/wall.png", window)
                 wall.append((col, row))
-    
-    for row in range(16):
-        for col in range(11):
-            if level[col][row] == "ENEMY" and frameCount >= 20:
-                if col > position[1]:
-                    enemyMove([row, col-1], row, col, window, level)
+            elif level[col][row] == "ENEMY":
+                drawObj(row, col, "sprites/ground.png", window)
+                drawObj(row, col, "sprites/goblin.png", window)
+                if col > position[1] and frameCount >= 60:
+                    level[col-1][row] = "ENEMY"
+                    level[col][row] = " "
                     frameCount = 0
+                elif col < position[1] and frameCount >= 60:
+                    level[col+1][row] = "ENEMY"
+                    level[col][row] = " "
+                    frameCount = 0
+                elif row < position[0] and frameCount >= 60:
+                    level[col][row+1] = "ENEMY"
+                    level[col][row] = " "
+                    frameCount = 0
+                elif row > position[0] and frameCount >= 60:
+                    level[col][row-1] = "ENEMY"
+                    level[col][row] = " "
+                    frameCount = 0
+            elif level[col][row] == " ":
+                drawObj(row, col, "sprites/ground.png", window)
 
     if wall != []:
-        return wall
+        return wall, frameCount
 
-def enemyMove(position, row, col, window, level):
-    pygame.draw.rect(window, (0,0,0), (row * tile, col * tile, tile, tile))
-    level[col+1][row] = " "
-    level[col][row] = "ENEMY"
-    print("moved?")
+def drawObj(row, col, imagePos, window):
+    image = pygame.image.load(f"{imagePos}")
+    image = pygame.transform.scale(image, (tile, tile))
+    window.blit(image, (row * tile, col * tile))
--- levelgen.py
+++ levelgen.py
@@ -1,2 +1,2 @@
 for i in range(1,18):
-    print(f'x', end="")
+    print(f'\" \",', end="")
