96a2eef6c9675e10ca82435511c617a163db14e4
Mark <marastodulka@gmail.com>
2024-11-27 19:16:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import pygame
-import inputHandler as IH
+import random
+import InputHandler as IH
 import levelLoader as LL
 import time
+from enemy import Enemy
 
 global image
 
 def main():
+    level = [["w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"],
+            ["1","2","3","w","5","6","7","8","9","10","11","12","13","14","15","16"],
+            ["1","2","3","w","5","6","7","8","9","10","11","12","13","14","15","x"],
+            ["1","2","3","w","5","6","7","8","9","10","11","12","13","14","15","16"],
+            ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"],
+            ["1","2","3","4","5","6","7","8","9","10","y","12","13","14","15","16"],
+            ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"],
+            ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"],
+            ["1","2","3","4","5","6","7","8","y","10","x","12","13","14","15","16"],
+            ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"],
+            ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]]
     pygame.init()
     clock = pygame.time.Clock()
     position = [0,0]
     
+    enemy_position = [random.randint(0, 15), random.randint(0, 11)]  #spawne enemaka
+    enemy = Enemy(
+        x=enemy_position[0] * 48,
+        y=enemy_position[1] * 48,
+        width=48,
+        height=48,
+        color=(255, 0, 0),
+        speed=2
+    )
     timerStart = time.time()
     pygame.display.set_caption("Rogue-Like game")
     width = 768  # 16
@@ -20,6 +42,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                return
         moveCharacter(position, window, color=(255,0,0))
         time2 = time.time()
 
@@ -32,16 +55,13 @@ def main():
         if time2 - timerStart >= 0.15:
             moveCharacter(position, window, color=(255,255,255))
             position = IH.inputHandler(position)
-            LL.loadLevel(window)
-            draw_img(window, position)
             timerStart = time.time()
+            
+        LL.loadLevel(window)
+        draw_img(window, position)
         
         pygame.display.flip()
         clock.tick(30)
-
-def moveCharacter(position, window, color):
-    tile = 48
-    pygame.draw.rect(window, color, (position[0] * tile, position[1] * tile, tile, tile))
 
 def moveCharacter(position, window, color):
     tile = 48
@@ -52,8 +72,6 @@ def draw_img(window, position):
     image = pygame.image.load("hero.png")
     image = pygame.transform.scale(image, (tile, tile))
     window.blit(image, (position[0] * tile, position[1] * tile))
-
-
 
 if __name__ == "__main__":
     main()
