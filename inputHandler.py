66df2a269387e60003f3615a7b97a5597f8fbe51
Mark <marastodulka@gmail.com>
2024-11-27 19:31:30 +0100

    Update inputHandler.py

--- inputHandler.py
+++ inputHandler.py
@@ -1,21 +1,33 @@
 import pygame
 
-def inputHandler(position):
+def inputHandler(position, wall):
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_a]:
         if position[0] != 0:
+            for i in range(len(wall)):
+                if position[0] - 1 == wall[i][1] and position[1] == wall[i][0]:
+                    return position
             position[0] -= 1
         return position
-    elif keys[pygame.K_RIGHT]:
+    elif keys[pygame.K_d]:
         if position[0] < 15:
+            for i in range(len(wall)):
+                if position[0] + 1 == wall[i][1] and position[1] == wall[i][0]:
+                    return position
             position[0] += 1
         return position
-    elif keys[pygame.K_UP]:
+    elif keys[pygame.K_w]:
         if position[1] != 0:
+            for i in range(len(wall)):
+                if position[0] == wall[i][1] and position[1] - 1 == wall[i][0]:
+                    return position
             position[1] -= 1
         return position
-    elif keys[pygame.K_DOWN]:
+    elif keys[pygame.K_s]:
         if position[1] < 11:
+            for i in range(len(wall)):
+                if position[0] == wall[i][1] and position[1] + 1 == wall[i][0]:
+                    return position
             position[1] += 1
         return position
     else:
