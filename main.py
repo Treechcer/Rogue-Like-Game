f01b0baa71323e9bc7fae8494171ffe088b51663
Treecher <martin.vrobel308@gmail.com>
2024-11-27 18:18:00 +0100

    accidentally merged it incorectly

--- main.py
+++ main.py
@@ -23,8 +23,6 @@ def main():
 
     wall = []
 
-    position = [0,0]
-    
     timerStart = time.time()
     pygame.display.set_caption("Rogue-Like game")
     width = 768  # 16 tileset
@@ -35,14 +33,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-        moveCharacter(position, window, color=(255,0,0))
-        time2 = time.time()
 
-        if time2 - timerStart >= 0.15:
-            moveCharacter(position, window, color=(255,255,255))
-            position = IH.inputHandler(position)
-            moveCharacter(position, window, color=(255,0,0))
-            timerStart = time.time()
+        time2 = time.time()
 
         if time2 - timerStart >= 0.15:
             moveCharacter(position, window, color=(255,255,255))
@@ -53,11 +45,7 @@ def main():
 
 
         pygame.display.flip()
-        clock.tick(30)
-
-def moveCharacter(position, window, color):
-    tile = 48
-    pygame.draw.rect(window, color, (position[0] * tile, position[1] * tile, tile, tile))
+        clock.tick(60)
 
 def moveCharacter(position, window, color):
     tile = 48
