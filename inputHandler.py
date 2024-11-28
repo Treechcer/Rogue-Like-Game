00b7f60b7cb990fb8df2f46656d07ee2ce01f745
Mark <marastodulka@gmail.com>
2024-11-28 19:16:28 +0100

    obrovské zjednodušení

--- inputHandler.py
+++ inputHandler.py
@@ -1,34 +1,20 @@
 import pygame
 
 def inputHandler(position, wall):
+    new_position = position[:]
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        if position[0] != 0:
-            for i in range(len(wall)):
-                if position[0] - 1 == wall[i][1] and position[1] == wall[i][0]:
-                    return position
-            position[0] -= 1
-        return position
-    elif keys[pygame.K_d]:
-        if position[0] < 15:
-            for i in range(len(wall)):
-                if position[0] + 1 == wall[i][1] and position[1] == wall[i][0]:
-                    return position
-            position[0] += 1
-        return position
-    elif keys[pygame.K_w]:
-        if position[1] != 0:
-            for i in range(len(wall)):
-                if position[0] == wall[i][1] and position[1] - 1 == wall[i][0]:
-                    return position
-            position[1] -= 1
-        return position
-    elif keys[pygame.K_s]:
-        if position[1] < 11:
-            for i in range(len(wall)):
-                if position[0] == wall[i][1] and position[1] + 1 == wall[i][0]:
-                    return position
-            position[1] += 1
-        return position
-    else:
-        return position
+    
+    if keys[pygame.K_a]: 
+        if new_position[0] > 0 and not any(new_position[0] - 1 == w[1] and new_position[1] == w[0] for w in wall):
+            new_position[0] -= 1
+    elif keys[pygame.K_d]: 
+        if new_position[0] < 15 and not any(new_position[0] + 1 == w[1] and new_position[1] == w[0] for w in wall):
+            new_position[0] += 1
+    elif keys[pygame.K_w]: 
+        if new_position[1] > 0 and not any(new_position[0] == w[1] and new_position[1] - 1 == w[0] for w in wall):
+            new_position[1] -= 1
+    elif keys[pygame.K_s]: 
+        if new_position[1] < 11 and not any(new_position[0] == w[1] and new_position[1] + 1 == w[0] for w in wall):
+            new_position[1] += 1
+
+    return new_position
