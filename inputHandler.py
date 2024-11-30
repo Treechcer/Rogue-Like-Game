4f651386224c33b16ae736075a1cbcdd1d28a16f
Treecher <martin.vrobel308@gmail.com>
2024-11-30 12:08:11 +0100

    fixed bug where you could move in insane speeds with arrow keys

--- inputHandler.py
+++ inputHandler.py
@@ -2,7 +2,7 @@ import pygame
 
 def inputHandler(position, wall, t1, t2):
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] or keys[pygame.K_a] and t2 - t1 >= 0.15:
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and t2 - t1 >= 0.15:
         if position[0] != 0:
             for i in range(len(wall)):
                 if position[0] - 1 == wall[i][1] and position[1] == wall[i][0]:
@@ -10,7 +10,7 @@ def inputHandler(position, wall, t1, t2):
             position[0] -= 1
             position[2] = True
         return position
-    elif keys[pygame.K_RIGHT] or keys[pygame.K_d] and t2 - t1 >= 0.15:
+    elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and t2 - t1 >= 0.15:
         if position[0] < 15:
             for i in range(len(wall)):
                 if position[0] + 1 == wall[i][1] and position[1] == wall[i][0]:
@@ -18,7 +18,7 @@ def inputHandler(position, wall, t1, t2):
             position[0] += 1
             position[2] = True
         return position
-    elif keys[pygame.K_UP] or keys[pygame.K_w] and t2 - t1 >= 0.15:
+    elif (keys[pygame.K_UP] or keys[pygame.K_w]) and t2 - t1 >= 0.15:
         if position[1] != 0:
             for i in range(len(wall)):
                 if position[0] == wall[i][1] and position[1] - 1 == wall[i][0]:
@@ -26,7 +26,7 @@ def inputHandler(position, wall, t1, t2):
             position[1] -= 1
             position[2] = True
         return position
-    elif keys[pygame.K_DOWN] or keys[pygame.K_s] and t2 - t1 >= 0.15:
+    elif (keys[pygame.K_DOWN] or keys[pygame.K_s]) and t2 - t1 >= 0.15:
         if position[1] < 11:
             for i in range(len(wall)):
                 if position[0] == wall[i][1] and position[1] + 1 == wall[i][0]:
